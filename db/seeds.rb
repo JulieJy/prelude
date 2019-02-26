30034f4ce33da4edb4fd0ada243a089464abcbee
Pedro Matanzo <pedromatanzo@gmail.com>
2019-02-26 18:20:52 +0100

    change the seed, add 4 games, theres NO user picture

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 p 'I destroy all users'
 User.destroy_all
+p 'I destroy all games'
+Game.destroy_all
+
 
 p 'I create 2 users'
 user1 = User.create!(
@@ -15,7 +18,8 @@ user1 = User.create!(
   email: "pedro@pedro.com",
   address: "Madrid",
   phone_number:"555-345-322",
-  password: "secret")
+  password: "secret"
+  )
 
 
 user2 = User.create!(
@@ -26,3 +30,63 @@ user2 = User.create!(
   phone_number:"555-345-322",
   password: "secret")
 
+
+
+user3 = User.create!(
+  first_name: "Hugo",
+  last_name: "Plé",
+  email: "hugo@hugo.com",
+  address: "20 rue des capucins, Lyon",
+  phone_number:"555-345-322",
+  password: "secret")
+
+
+
+user4 = User.create!(
+  first_name: "David",
+  last_name: "Habault",
+  email: "david@david.com",
+  address: "20 rue des capucins, Lyon",
+  phone_number:"555-345-322",
+  password: "secret")
+
+
+p 'creating games...'
+game1 = Game.create!(
+
+  name:"Les aventuriers du rail",
+  nb_player: 4,
+  description: "Les Aventuriers du Rail - Europe  est le deuxième volume de la célèbre série des Aventuriers du Rail dont la version de base se joue sur une carte des USA. Plus qu'une nouvelle carte, cette version incorpore de nouvelles routes (tunnels et ferries), et de nouvelles pièces de jeu, les gares.",
+  picture: "https://www.espritjeu.com/upload/image/les-aventuriers-du-rail---europe-p-image-59803-grande.jpg",
+  duration: 30,
+  category: "strategy"
+)
+
+game2 = Game.create!(
+  name:"7 wonders",
+  nb_player: 4,
+  description: "L'Antiquité et ses merveilles. Revivez l'épopée des grandes constructions avec ce jeu de cartes et de stratégie !",
+  picture: "https://cdn3.philibertnet.com/310399-large_default/7-wonders-vf.jpg",
+  duration: 120,
+  category: "strategy"
+  )
+
+game3 = Game.create!(
+  name:"Code Names",
+  nb_player: 4,
+  description: "Jouez les espions et retrouvez vos alliés avec des Noms de Code! Codenames est un jeu d'expression, d'association d'idées et de déduction qui se joue en équipe.",
+  picture: "https://static.fnac-static.com/multimedia/Images/FR/NR/c1/b8/78/7911617/1540-1/tsp20160826112220/Codenames-Iello.jpg",
+  duration: 120,
+  category: "ambiance"
+  )
+
+
+game4 = Game.create!(
+  name:"Tu te mets combien ?",
+  nb_player: 4,
+  description: "Évalue tes connaissances de 1 à 10 et réponds aux questions !",
+  picture: "https://cdn3.philibertnet.com/419835-large_default/ttmc-tu-te-mets-combien-.jpg",
+  duration: 90,
+  category: "junior"
+  )
+p '4 games created'
