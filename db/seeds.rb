4c969aae494611ad6c85937131205ae7603f7486
Pedro Matanzo <pedromatanzo@gmail.com>
2019-02-26 15:16:54 +0100

    add 2 users in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p 'I destroy all users'
+User.destroy_all
+
+p 'I create 2 users'
+user1 = User.create!(
+  first_name: "Pedro",
+  last_name: "Matanzo",
+  email: "pedro@pedro.com",
+  address: "Madrid",
+  phone_number:"555-345-322",
+  password: "secret")
+
+
+user2 = User.create!(
+  first_name: "Julie",
+  last_name: "Jaumary",
+  email: "julie@julie.com",
+  address: "20 rue des capucins, Lyon",
+  phone_number:"555-345-322",
+  password: "secret")
+
