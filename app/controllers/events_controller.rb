ab00c4d893ad1074cb4ab2c7509500f1c627749e
david habault <david.habault@gmail.com>
2019-02-26 17:33:35 +0100

    add show, new and create for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,33 @@
 class EventsController < ApplicationController
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
   def new
+    @event = Event.new
   end
 
   def create
-  end
+    @event = Event.new(event_params)
+    # @user = User.find(params[:user_id])
+    # @user.role = organizer
 
-  def show
+    if @event.save
+      Participant.create!(event: @event, user: current_user, role: 'organizer')
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :time, :place, :nb_person)
+  end
+
 end
