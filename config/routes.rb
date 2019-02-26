6d0c920486186239f41b81dfe05bfea9ffe96082
Julie-kiwi <jaumary.julie@gmail.com>
2019-02-26 18:13:10 +0100

    routes ok (nested library into user)

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: :show
+  resources :users, only: :show do
+    resources :libraries, only: [:index, :create, :destroy]
+  end
   resources :games, only: [:new, :create, :show]
   resources :event, only: [:new, :create, :show]
   resources :participants, only: [:index, :new, :create, :destroy]
-  resources :libraries, only: [:index, :create, :destroy]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
