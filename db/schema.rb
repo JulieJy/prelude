f67db9e07d4db104280b0849f3edb45b7d2bf5d0
Pedro Matanzo <pedromatanzo@gmail.com>
2019-02-27 12:18:44 +0100

    add columns for latitude and longitude

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_142601) do
+ActiveRecord::Schema.define(version: 2019_02_27_111755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2019_02_26_142601) do
     t.string "phone_number"
     t.string "address"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
